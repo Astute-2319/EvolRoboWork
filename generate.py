42cf585d53dd7c272288ff2385647a2744565047
Alex Stute <stute5000@gmail.com>
2024-02-02 18:48:34 -0500

    created function to Create_Robot

--- generate.py
+++ generate.py
@@ -2,17 +2,17 @@ import pyrosim.pyrosim as pyrosim
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    length = 1
-    width = 1
-    height = 1
 
-    x = 0
-    y = 0
-    z = 0.5
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
 
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
 
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+
+    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
 
     pyrosim.End()
 
 Create_World()
+Create_Robot()
